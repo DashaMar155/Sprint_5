5c3d790e528f92a57e437c1229b70fd31d266541
Dasha Marevskaya <maikop_krasnokamensk@mail.ru>
2025-06-28 23:12:22 +0300

    бновлены фикстуры и тесты регистрации/навигации

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from data import TestLinks, Credantial
 from generation_ep import generate_unique_email
 
 
-# Фикстура WebDriver
+# Фикстура для WebDriver
 @pytest.fixture
 def driver():
     options = Options()
@@ -16,25 +16,22 @@ def driver():
     driver.quit()
 
 
-# Фикстура: уникальный email
+# Фикстура: уникальный email (генератор)
 @pytest.fixture
 def test_email():
     return generate_unique_email()
 
 
-# Фикстура для регистрации пользователя
+# Фикстура: зарегистрированный пользователь
 @pytest.fixture
 def registered_user(driver, test_email):
-    password = Credantial.password
-    name = Credantial.name
-
     driver.get(TestLinks.registration_page_link)
-    AuthHelper.register(driver, test_email, password, name)
+    AuthHelper.register(driver, test_email, Credantial.password, Credantial.name)
     AuthHelper.confirm_successful_registration(driver)
-    return test_email, password
+    return test_email, Credantial.password
 
 
-# Фикстура для авторизованного пользователя
+# Фикстура: авторизованный пользователь
 @pytest.fixture
 def authorized_user(driver, registered_user):
     email, password = registered_user
@@ -42,5 +39,3 @@ def authorized_user(driver, registered_user):
     AuthHelper.login(driver, email, password)
     AuthHelper.confirm_successful_login(driver)
     return email, password
-
-
